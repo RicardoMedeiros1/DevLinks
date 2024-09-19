5d0de393f6193a34725ee351cef51ba810ab09e0
RicardoMedeiros1 <meedeirosricardo95@gmail.com>
2024-09-19 14:02:56 -0300

    editando e adiconando foto no codigo

--- assents/script.js
+++ assents/script.js
@@ -1,9 +1,19 @@
 function toggleMode(){
     const html = document.documentElement
+    html.classList.toggle("light")
+    
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
 
+
+    // substituir a imagem
     if(html.classList.contains('light')){
-      html.classList.remove('light')
+
+    //se ativer light mode, adicionar a imagem light
+      img.setAttribute("src", "./assents/avatar-light.png")  
     } else {
-      html.classList.add('light')
+      // se tiver sem o light mode, manter a imagem normal
+      img.setAttribute("src", "./assents/avatar.png")
     }
 }
+
